9f59234d6c027b0b1652accddac9bada25754493
Artimas <mblanzy@hotmail.com>
2019-01-30 20:14:36 -0600

    signin, username, password, email working

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True 
 
-@app.route("/signin", methods=["POST"])
+@app.route("/")
+def signin():
+    return render_template('edit.html')
+
+@app.route("/", methods=["POST"])
 def user_signin():
+    username = request.form["username"]
+    email = request.form["email"]
+    password =  request.form["password"]
+    password_v =  request.form["password_v"]
     username_error = ""
     password_error = ""
     email_error = ""
@@ -14,11 +22,11 @@ def user_signin():
     if not username:
         username_error = "Please input a username"
 
-    elif len(username) > 3 or len(username) < 20:
+    elif len(username) < 3 or len(username) > 20:
         username_error = "Please input a valid username between 3 and 20 characters."
 
     else:
-        username = username
+        username = request.form["username"]
 
     if not password:
         password_error = "Please input a password"
@@ -29,25 +37,27 @@ def user_signin():
     elif len(password) < 3 or len(password) > 20:
         password_error = "Please input a password between 3 and 20 characters"
 
-    else:
-        continue
+    if not email and  not username_error and not password_error and not email_error:
+        username = request.form["username"]
+        return redirect("/signin?username={0}".format(username))
+    
+    elif "@" in email and "." in email and not " " in email:
+        username = request.form["username"]
+        return redirect("/signin?username={0}".format(username))
 
-    if not username_error and not password_error and not email_error:
-        username = username
-        return render_template("signin.html", username=username)
     else:
-        render_template("edit.html", 
+        email_error = "Please enter a valid email."
+
+    return render_template("edit.html", 
         username_error = username_error,
         password_error = password_error,
         email_error = email_error,
         username = username,
         email = email)
 
-@app.route("/")
-def index():
-    username = ""
-    email = ""
-    encoded_error = request.args.get("error")
-    return render_template('edit.html', username=username, email=email)
+@app.route('/signin')
+def valid_signin():
+    username = request.args.get("username")
+    return render_template("signin.html", username = username)
 
 app.run()
